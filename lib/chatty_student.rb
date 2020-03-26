ffb7dcdba6a022e09bd985228f1d9ffdeee8c0b7
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-03-26 01:02:35 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -5,6 +5,9 @@ class ChattyStudent < Student
     "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
     
     def raised_hand
-     10.times do
+     10.times do 
+       super
+     end
     end
+    
 end
